5c14f1a4788978eee2bf23c6cd9688524dd37aaf
davi gerson <dgas1@discente.ifpe.edu.br>
2025-09-26 21:11:24 -0300

    feito o footer

---
+++ script.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  slidesPerView: 2,
+  spaceBetween: 10,
+  loop: true,
+  breakpoints: {
+    600: {
+      loop: true,
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+  },
+});
